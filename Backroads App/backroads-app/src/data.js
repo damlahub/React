e6e81bb4d2595d4d14a1af59095f06f1bd07481e
damlaoksel <damlaaoksel@gmail.com>
2023-11-25 20:15:25 +0300

    Backroads App | Navbar yapıldı.

---
+++ Backroads App/backroads-app/src/data.js
@@ -0,0 +1,39 @@
+export const pageLinks = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '/'
+    },
+    {
+        id: 2,
+        name: 'About',
+        href: '/about'
+    },
+    {
+        id: 3,
+        name: 'Services',
+        href: '/services'
+    },
+    {
+        id: 4,
+        name: 'Tours',
+        href: '/tours'
+    }
+];
+export const pageIcons = [
+    {
+        id: 1,
+        href: 'https://www.twitter.com',
+        icon: "fab fa-facebook"
+    },
+    {
+        id: 2,
+        href: 'https://www.twitter.com',
+        icon: "fab fa-twitter"
+    },
+    {
+        id: 3,
+        href: 'https://www.twitter.com',
+        icon: "fab fa-squarespace"
+    },
+];
