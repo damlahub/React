ab2996519eb482d93fcd93a55a41c4ff4fc4c2ad
damlaoksel <damlaaoksel@gmail.com>
2023-12-14 19:53:04 +0300

    useState object ögrenildi

--- Advanced_React/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ Advanced_React/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,20 +1,28 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName]=useState('Damla');
-  const [age, setAge]=useState('22');
-  const [hobby, setHobby]=useState('Scream at the computer.');
+
+  const [person, setPerson] = useState({
+    name:'Damla',
+    age: 22,
+    hobby:"Scream at the computer."
+  });
+
+  // const [name, setName]=useState('Damla');
+  // const [age, setAge]=useState('22');
+  // const [hobby, setHobby]=useState('Scream at the computer.');
 
   const displayPerson=()=>{
-    setName("John");
-    setAge(42);
-    setHobby("Read books.");
+    // setName("John");
+    // setAge(42);
+    // setHobby("Read books.");
+    setPerson({...person,name:'John'}); // Tüm özellikleri aldık sadece name özelliğini değiştirmek istedik.
   }
   return(
     <div>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h3>Enjoys : {hobby}</h3>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoys : {person.hobby}</h3>
       <button onClick={displayPerson} className='btn'>Show John</button>
     </div>
   );
